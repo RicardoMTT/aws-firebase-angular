fff9cab42ee73e7661630846c440b7ba5022bd90
Ricardo Tovar <tricardo003@gmail.com>
2024-04-28 13:28:31 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Tutorial, TutorialService } from './service/tutorial.service.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular15-firebase';
+  tutorials?: Tutorial[];
+  currentTutorial?: Tutorial;
+  currentIndex = -1;
+  title = '';
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  refreshList(): void {
+    this.currentTutorial = undefined;
+    this.currentIndex = -1;
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    const tutorial  = new Tutorial();
+    tutorial.key="123";
+    tutorial.description="desc";
+    this.tutorialService.create(tutorial).then(() => {
+      console.log('Created new item successfully!');
+      // this.submitted = true;
+    });
+    this.tutorialService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      console.log('data',data);
+
+      this.tutorials = data;
+    });
+  }
+
+  setActiveTutorial(tutorial: Tutorial, index: number): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll()
+      .then(() => this.refreshList())
+      .catch(err => console.log(err));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { TutorialService } from './service/tutorial.service.component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(
+      {
+          apiKey: "AIzaSyCZ9mZdRkz52jzHys_AwguSSkNmdMXe1Wo",
+          authDomain: "angular15firebase-4f28d.firebaseapp.com",
+          projectId: "angular15firebase-4f28d",
+          storageBucket: "angular15firebase-4f28d.appspot.com",
+          messagingSenderId: "649669028198",
+          appId: "1:649669028198:web:ab99d627c240c522acc0a7"
+        }
+    ),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
+    // const firebaseConfig = {
+    //   apiKey: "AIzaSyCZ9mZdRkz52jzHys_AwguSSkNmdMXe1Wo",
+    //   authDomain: "angular15firebase-4f28d.firebaseapp.com",
+    //   projectId: "angular15firebase-4f28d",
+    //   storageBucket: "angular15firebase-4f28d.appspot.com",
+    //   messagingSenderId: "649669028198",
+    //   appId: "1:649669028198:web:ab99d627c240c522acc0a7"
+    // };
   ],
-  providers: [],
+  providers: [
+    TutorialService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
